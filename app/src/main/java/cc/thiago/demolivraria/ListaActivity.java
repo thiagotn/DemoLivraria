76ff28114c7ce44698b46f37ed3f2ca422da0c46
unknown <rm49824@L2201MICRO22.fiap.com.br>
2015-11-26 23:03:12 -0200

    listagem

--- app/src/main/java/cc/thiago/demolivraria/ListaActivity.java
+++ app/src/main/java/cc/thiago/demolivraria/ListaActivity.java
@@ -22,7 +22,7 @@ public class ListaActivity extends AppCompatActivity {
         Livro livro = new Livro();
         livro.setDescricao("Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.\n");
         livro.setTitulo("Android é TOP");
-        livro.setEditora("");
+        livro.setEditora("AAA");
         livro.setIsbn("1121212");
         livro.setAutor("Google");
         crud.inserir(livro);
